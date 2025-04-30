e0b7770c1da53d56881acc0df89d7e283512f494
anesukuveya <anesugail16@gmail.com>
2025-04-30 11:05:26 +0200

    Project set-up

--- src/test/java/com/project/anesu/ecommerce/stockmanagementservice/entity/Product.java
+++ src/test/java/com/project/anesu/ecommerce/stockmanagementservice/entity/Product.java
@@ -1,2 +1,28 @@
-package com.project.anesu.ecommerce.stockmanagementservice.entity;public class Product {
+package com.project.anesu.ecommerce.stockmanagementservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private String productDescription;
+    private double price;
+    private int size;
+    private String colour;
+
+    @ManyToOne
+    private Category category;
+
 }
--- src/test/java/com/project/anesu/ecommerce/stockmanagementservice/model/repository/ProductRepository.java
+++ src/test/java/com/project/anesu/ecommerce/stockmanagementservice/model/repository/ProductRepository.java
@@ -1,2 +1,9 @@
-package com.project.anesu.ecommerce.stockmanagementservice.model.repository;public interface ProductRepository {
+package com.project.anesu.ecommerce.stockmanagementservice.model.repository;
+
+import com.project.anesu.ecommerce.stockmanagementservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
