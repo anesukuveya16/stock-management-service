e3a29080d0eaf376219e39f60d7df582bc4d23f5
anesukuveya <anesugail16@gmail.com>
2025-06-23 15:18:29 +0200

    test: fix ProductServiceTest class.

--- src/test/java/com/project/anesu/ecommerce/stockmanagementservice/integrationTests/ProductServiceTest.java
+++ src/test/java/com/project/anesu/ecommerce/stockmanagementservice/integrationTests/ProductServiceTest.java
@@ -1,7 +1,6 @@
 package com.project.anesu.ecommerce.stockmanagementservice.integrationTests;
 
-import static com.project.anesu.ecommerce.stockmanagementservice.controller.StockControllerEndpoints.CREATE_PRODUCT;
-import static com.project.anesu.ecommerce.stockmanagementservice.controller.StockControllerEndpoints.LANDING_PAGE;
+import static com.project.anesu.ecommerce.stockmanagementservice.controller.StockControllerEndpoints.*;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.project.anesu.ecommerce.stockmanagementservice.entity.Category;
@@ -37,8 +36,215 @@ class ProductServiceTest {
   @Test
   void shouldAddNewProductSuccessfully() {
 
-    String addProduct =
+    String addProduct = getAddProduct();
+
+    String addedProduct = String.format(addProduct, categoryId);
+
+    RestAssured.given()
+        .contentType(ContentType.JSON)
+        .body(addedProduct)
+        .when()
+        .post(LANDING_PAGE + CREATE_PRODUCT)
+        .then()
+        .statusCode(200)
+        .body("productName", equalTo("Denim Jacket"))
+        .body("price", equalTo(69.99f))
+        .body("size", equalTo(36));
+  }
+
+  @Test
+  void shouldSuccessfullyRetrieveProductByGivenProductId() {
+
+    String addProduct = getAddProduct();
+
+    String addedProduct = String.format(addProduct, categoryId);
+
+    Integer productId =
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(addedProduct)
+            .when()
+            .post(LANDING_PAGE + CREATE_PRODUCT)
+            .then()
+            .statusCode(200)
+            .body("productName", equalTo("Denim Jacket"))
+            .body("price", equalTo(69.99f))
+            .body("size", equalTo(36))
+            .extract()
+            .path("id");
+
+    String retrievedProduct =
+        """
+              {
+                "productName": "Denim Jacket",
+                "productDescription": "Blue cropped denim jacket.",
+                "price": 69.99,
+                "size": 36,
+                "colour": "Blue",
+                "category": {
+                   "id": %d
+                    }
+              }
+              """;
+
+    RestAssured.given()
+        .contentType(ContentType.JSON)
+        .body(retrievedProduct)
+        .when()
+        .get(LANDING_PAGE + GET_PRODUCT_BY_ID, productId)
+        .then()
+        .statusCode(200)
+        .body("productName", equalTo("Denim Jacket"))
+        .body("price", equalTo(69.99f))
+        .body("colour", equalTo("Blue"));
+  }
+
+  @Test
+  void shouldSuccessfullyUpdateProductSuccessfully() {
+
+    String addProduct = getAddProduct();
+
+    String addedProduct = String.format(addProduct, categoryId);
+
+    Integer productId =
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(addedProduct)
+            .when()
+            .post(LANDING_PAGE + CREATE_PRODUCT)
+            .then()
+            .statusCode(200)
+            .body("productName", equalTo("Denim Jacket"))
+            .body("price", equalTo(69.99f))
+            .body("size", equalTo(36))
+            .extract()
+            .path("id");
+
+    String updateExistingProduct =
+        """
+                      {
+                        "productName": "Denim Jacket",
+                        "productDescription": "Cropped denim jacket.",
+                        "price": 29.99,
+                        "size": 36,
+                        "colour": "baby blue",
+                        "category": {
+                           "id": %d
+                            }
+                      }
+                      """;
+
+    RestAssured.given()
+        .contentType(ContentType.JSON)
+        .body(updateExistingProduct)
+        .when()
+        .put(LANDING_PAGE + UPDATE_PRODUCT, productId)
+        .then()
+        .statusCode(200)
+        .body("productDescription", equalTo("Cropped denim jacket."))
+        .body("colour", equalTo("baby blue"));
+  }
+
+  @Test
+  void shouldDeleteProductFromDatabase() {
+
+    String addProduct = getAddProduct();
+    String addedProduct = String.format(addProduct, categoryId);
+
+    Integer productId =
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(addedProduct)
+            .when()
+            .post(LANDING_PAGE + CREATE_PRODUCT)
+            .then()
+            .statusCode(200)
+            .body("productName", equalTo("Denim Jacket"))
+            .body("price", equalTo(69.99f))
+            .body("size", equalTo(36))
+            .extract()
+            .path("id");
+
+    String deleteProduct =
+        """
+                      {
+                        "productName": "Denim Jacket",
+                        "productDescription": "Blue cropped denim jacket.",
+                        "price": 69.99,
+                        "size": 36,
+                        "colour": "Blue",
+                        "category": {
+                           "id": %d
+                            }
+                      }
+                      """;
+
+    RestAssured.given()
+        .contentType(ContentType.JSON)
+        .body(deleteProduct)
+        .when()
+        .get(LANDING_PAGE + DELETE_PRODUCT, productId)
+        .then()
+        .statusCode(200);
+  }
+
+
+  @Test
+  void shouldRetrieveAllProductsFromDatabase() {
+
+    String addProductRequestBody = getAddProduct();
+    String addedProduct = String.format(addProductRequestBody, categoryId);
+
+    RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(addedProduct)
+            .when()
+            .post(LANDING_PAGE + CREATE_PRODUCT)
+            .then()
+            .statusCode(200)
+            .body("productName", equalTo("Denim Jacket"))
+            .body("price", equalTo(69.99f))
+            .body("size", equalTo(36));
+
+    String addedProductTwoRequestBody =
             """
+                    {
+                      "productName": "Long formal skirt",
+                      "productDescription": "Knee length summer skirt.",
+                      "price": 9.99,
+                      "size": 38,
+                      "colour": "Green",
+                      "category": {
+                         "id": %d
+                          }
+                    }
+                    """;
+    String addedProductTwo = String.format(addedProductTwoRequestBody, categoryId);
+
+
+    RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(addedProductTwo)
+            .when()
+            .post(LANDING_PAGE + CREATE_PRODUCT)
+            .then()
+            .statusCode(200)
+            .body("productName", equalTo("Long formal skirt"))
+            .body("price", equalTo(9.99f))
+            .body("size", equalTo(38));
+
+    RestAssured.given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get(LANDING_PAGE + GET_ALL_PRODUCTS)
+            .then()
+            .statusCode(200);
+  }
+
+
+
+  private static String getAddProduct() {
+    return """
                             {
                               "productName": "Denim Jacket",
                               "productDescription": "Blue cropped denim jacket.",
@@ -46,20 +252,11 @@ class ProductServiceTest {
                               "size": 36,
                               "colour": "Blue",
                               "category": {
-                                "id": %
+                                "id": %d
                               }
           }
           """;
-
-    String finalProduct = String.format(addProduct, categoryId);
-
-    RestAssured.given()
-        .contentType(ContentType.JSON)
-        .body(finalProduct)
-        .when()
-        .post(LANDING_PAGE + CREATE_PRODUCT)
-        .then()
-        .statusCode(200)
-        .body("productName", equalTo("Denim Jacket"));
   }
+
+
 }
