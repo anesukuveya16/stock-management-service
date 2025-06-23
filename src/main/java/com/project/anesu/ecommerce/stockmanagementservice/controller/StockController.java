ab74e1748dfa51a4da7812a30c2e536a3335116e
anesukuveya <anesugail16@gmail.com>
2025-06-23 13:44:54 +0200

    minor code refactor in controller class.

--- src/main/java/com/project/anesu/ecommerce/stockmanagementservice/controller/StockController.java
+++ src/main/java/com/project/anesu/ecommerce/stockmanagementservice/controller/StockController.java
@@ -12,7 +12,6 @@ import com.project.anesu.ecommerce.stockmanagementservice.service.exception.LowS
 import com.project.anesu.ecommerce.stockmanagementservice.service.util.InventoryValidator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,56 +26,67 @@ public class StockController {
   private final CategoryServiceImpl categoryService;
   private final InventoryValidator inventoryValidator;
 
-  @PostMapping(StockControllerEndpoints.CREATE_PRODUCT)
-  public Product addProduct(@RequestBody Product product) {
-    return productService.addNewProduct(product);
-  }
-
-  @GetMapping(StockControllerEndpoints.GET_PRODUCT_BY_ID)
-  public Optional<Product> getProductById(@PathVariable Long id) {
-    return productService.getProductById(id);
-  }
-
-  @GetMapping(StockControllerEndpoints.GET_ALL_PRODUCTS)
-  public List<Product> getAllProducts() {
-    return productService.getAllProducts();
-  }
-
-  @PutMapping(StockControllerEndpoints.UPDATE_PRODUCT)
-  public Product updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct)
-      throws InvalidProductException {
-    return productService.updateProduct(id, updatedProduct);
-  }
-
-  @DeleteMapping(StockControllerEndpoints.DELETE_PRODUCT)
-  public void deleteProduct(@PathVariable Long id) throws InvalidProductException {
-    productService.deleteProduct(id);
-  }
-
-  @PostMapping(StockControllerEndpoints.CREATE_CATEGORY)
+  @PostMapping(CREATE_CATEGORY)
   public Category createCategory(@RequestBody Category category) {
     return categoryService.createNewCategory(category);
   }
 
-  @GetMapping(StockControllerEndpoints.GET_CATEGORY_BY_ID)
-  public Optional<Category> getCategoryById(@PathVariable Long id) {
-    return categoryService.getCategoryById(id);
+  @GetMapping(GET_CATEGORY_BY_ID)
+  public Category getCategoryById(@PathVariable Long categoryId) {
+    return categoryService.getCategoryById(categoryId);
   }
 
-  @DeleteMapping(StockControllerEndpoints.DELETE_CATEGORY)
-  public void deleteCategory(@PathVariable Long id) throws CategoryNotFoundException {
-    categoryService.deleteCategory(id);
+  @DeleteMapping(DELETE_CATEGORY)
+  public void deleteCategory(@PathVariable Long categoryId) throws CategoryNotFoundException {
+    categoryService.deleteCategory(categoryId);
   }
 
-  @PostMapping(StockControllerEndpoints.VALIDATE_AND_DEDUCT_PRODUCT_FROM_INVENTORY)
+  @PostMapping(CREATE_PRODUCT)
+  public Product addProduct(@RequestBody Product product) {
+    return productService.addNewProduct(product);
+  }
+
+  @GetMapping(GET_PRODUCT_BY_ID)
+  public Product getProductById(@PathVariable Long productId) {
+    return productService.getProductById(productId);
+  }
+
+  @GetMapping(GET_ALL_PRODUCTS)
+  public List<Product> getAllProducts() {
+    return productService.getAllProducts();
+  }
+
+  @PutMapping(UPDATE_PRODUCT)
+  public Product updateProduct(@PathVariable Long productId, @RequestBody Product updatedProduct)
+      throws InvalidProductException {
+    return productService.updateProduct(productId, updatedProduct);
+  }
+
+  @DeleteMapping(DELETE_PRODUCT)
+  public void deleteProduct(@PathVariable Long productId) throws InvalidProductException {
+    productService.deleteProduct(productId);
+  }
+
+  @PostMapping(VALIDATE_AND_DEDUCT_PRODUCT_FROM_INVENTORY)
   public ResponseEntity<String> validateInventory(
       @RequestBody List<Map<String, Object>> orderItems) {
     try {
       inventoryValidator.validateInventoryAvailability(orderItems);
-      return ResponseEntity.ok("Inventory successfully validated!");
+      return ResponseEntity.ok().build();
     } catch (InvalidProductException e) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     } catch (LowStockException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+  }
+
+  @PostMapping(ADD_RETURNED_INVENTORY)
+  public ResponseEntity<String> addReturnedInventory(
+      @RequestBody List<Map<String, Object>> returnOrderItems) {
+    try {
+      inventoryValidator.validateInventoryAvailability(returnOrderItems);
+      return ResponseEntity.ok().build();
+    } catch (InvalidProductException e) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
   }
--- src/main/java/com/project/anesu/ecommerce/stockmanagementservice/controller/StockControllerEndpoints.java
+++ src/main/java/com/project/anesu/ecommerce/stockmanagementservice/controller/StockControllerEndpoints.java
@@ -5,17 +5,18 @@ public class StockControllerEndpoints {
   public static final String LANDING_PAGE = "/api/stock";
 
   public static final String CREATE_PRODUCT = "/product";
-  public static final String UPDATE_PRODUCT = "/product/{id}";
-  public static final String GET_PRODUCT_BY_ID = "/product/{id}";
+  public static final String UPDATE_PRODUCT = "/product/{productId}";
+  public static final String GET_PRODUCT_BY_ID = "/product/{productId}";
   public static final String GET_ALL_PRODUCTS = "/products";
-  public static final String DELETE_PRODUCT = "/product/{id}";
+  public static final String DELETE_PRODUCT = "/product/{productId}";
 
   public static final String CREATE_CATEGORY = "/category";
-  public static final String GET_CATEGORY_BY_ID = "/category/{id}";
-  public static final String DELETE_CATEGORY = "/category/{id}";
+  public static final String GET_CATEGORY_BY_ID = "category/{categoryId}";
+  public static final String DELETE_CATEGORY = "/category/{categoryId}";
 
   public static final String VALIDATE_AND_DEDUCT_PRODUCT_FROM_INVENTORY =
       "/validate-and-deduct-product";
+  public static final String ADD_RETURNED_INVENTORY = "/add-returned-inventory";
 
   private StockControllerEndpoints() {}
 }
